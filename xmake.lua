dbe2c971f1057b0c8a4254f3d841ac0b6f6cd72f
zdzDesigner <1696498456@qq.com>
2023-06-07 10:11:58 +0800

    ~

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,36 @@
-task("quiet", function()
-  on_run(function()
-    os.exec("bochs -q")
+add_rules("mode.debug")
+
+
+target("hello", function()
+  set_default(false)
+  add_files("./*.asm")
+  on_load(function(target)
+    print("name: %s", target:name())
+    -- os.exec("nasm ./*.asm -o ./*.bin")
+    -- os.exec("dd if=hello.bin of=master.img bs=512 count=1 conv=notrunc")
+    -- os.exec("bochs -q")
   end)
 end)
 
-target("compile", function()
-  set_kind("binary")
-  add_files("./hello.asm")
 
-  after_build(function(target)
-    os.exec("dd if=hello.bin of=master.img bs=512 count=1 conv=notrunc")
-    -- task.run("quiet")
-  end)
-end)
+-- target("hello", function()
+--   set_default(false)
+--   on_build(function(target)
+--     print("name: %s", target:name())
+--     os.exec("nasm ./hello.asm -o ./hello.bin")
+--     os.exec("dd if=hello.bin of=master.img bs=512 count=1 conv=notrunc")
+--     os.exec("bochs -q")
+--   end)
+-- end)
+
+
+
+
+
+
+
+
+
+
+
+
